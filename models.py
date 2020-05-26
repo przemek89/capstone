4c9f8fefe7dfd0c25cf386c5a180f14633639d8c
Przemek Żbikowski <you@example.com>
2020-05-26 10:06:12 +0200

    created Movies and Artists models and ran DB migration

--- models.py
+++ models.py
@@ -1,15 +1,69 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
 from app import APP
 from flask_migrate import Migrate
 
 db_url = "postgres://postgres:postgres@localhost:5432/castingAgency"
 db = SQLAlchemy()
+db.init_app(APP)
 
 def setup_database(app, database_path=db_url):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    # db.create_all()
     migrate = Migrate(app, db)
+
+class Movies(db.Model):
+    __tablename__ = 'Venues'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    release_date = db.Column(db.Integer)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit(self)
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+class Artists(db.Model):
+    __tablename__ = 'Artists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(80))
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit(self)
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
