7347b530ccc95c8c1b09b04bd9bf9659d741ccb7
Przemek Żbikowski <you@example.com>
2020-05-26 09:35:10 +0200

    set up the connection to the database and initialised migrations using manage.py

--- models.py
+++ models.py
@@ -1,1 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app import APP
+from flask_migrate import Migrate
 
+db_url = "postgres://postgres:postgres@localhost:5432/castingAgency"
+db = SQLAlchemy()
+
+def setup_database(app, database_path=db_url):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+    migrate = Migrate(app, db)
