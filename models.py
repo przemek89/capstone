aefbc3e39921d774c8aef19feba107352ab459e0
Przemek Żbikowski <you@example.com>
2020-05-28 10:07:09 +0200

    created a set up for tests and tests for all GET endpoints

--- models.py
+++ models.py
@@ -1,23 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
-from app import APP
 from flask_migrate import Migrate
 
-db_url = "postgres://postgres:postgres@localhost:5432/castingAgency"
+db_url = "postgres://postgres:123456@localhost:5432/castingAgency"
 db = SQLAlchemy()
-db.init_app(APP)
+#db.init_app(APP)
 
 def setup_database(app, database_path=db_url):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    # db.create_all()
+    db.create_all()
     migrate = Migrate(app, db)
 
 class Movies(db.Model):
-    __tablename__ = 'Venues'
+    __tablename__ = 'Movies'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
--- test_app.py
+++ test_app.py
@@ -1,1 +1,120 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
 
+from app import create_app
+from models import setup_database, Artists, Movies
+
+
+class CastingAgencyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.token_assistant = os.environ['assistant_token']
+        self.token_director = os.environ['director_token']
+        self.token_producer = os.environ['producer_token']
+        self.actor = {
+            "name": "Tom Cruise",
+            "age": 50,
+            "gender": "male"
+        }
+        self.movie = {
+            "title": "Top Gun",
+            "release_date": 1986
+        }
+        self.app = create_app()
+        self.client = self.app.test_client
+        setup_database(self.app, self.database_path)
+        self.db.drop_all()
+        self.db.create_all()
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    #GET actors success casting asistant
+    def test_get_artists_casting_assistant(self):
+        res = self.client().get('/Artists', headers={
+            "Authorization": 'bearer ' + self.token_assistant})
+        body = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(body['success'], True)
+
+    #GET actors failure
+    def test_get_artists_no_authorisation(self):
+        res = self.client().get('/Artists')
+        body = json.loads(res.data)
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(body['success'], False)
+
+    #GET movies success casting asistant
+    def test_get_movies_casting_assistant(self):
+        res = self.client().get('/Movies', headers={
+            "Authorization": 'bearer ' + self.token_assistant})
+        body = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(body['success'], True)
+
+    #GET movies failure casting asistant
+    def test_get_movies_no_authorisation(self):
+        res = self.client().get('/Artists')
+        body = json.loads(res.data)
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(body['success'], False)
+
+    #GET actor success casting director
+    def test_get_actor_casting_director(self):
+        #post actor
+        res = self.client().post('/Artists', headers={"Authorization": 'bearer ' + self.token_director}, json=self.actor)
+        #get actor
+        res = self.client().get('/Artists/1', headers={
+            "Authorization": 'bearer ' + self.token_director})
+        body = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(body['success'], True)
+        self.assertEqual(data['actor'], {'id': 1, 'name': 'Tom Cruise', 'age': 50, 'gender': 'male'})
+
+    #GET actor failure casting director
+    def test_get_actor_failure(self):
+        res = self.client().get('/Artists/123456789', headers={
+            "Authorization": 'bearer ' + self.token_director})
+        body = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(body['success'], False)
+        self.assertEqual(body['message'], 'resource not found')
+
+    #GET movie success casting director
+    def test_get_movie_casting_director(self):
+        #post movie
+        res = self.client().post('/Movies', headers={"Authorization": 'bearer ' + self.token_director}, json=self.movie)
+        #get movie
+        res = self.client().get('/Movies/1', headers={
+            "Authorization": 'bearer ' + self.token_director})
+        body = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(body['success'], True)
+        self.assertEqual(data['actor'], {'id': 1, 'title': 'Top Gun', 'release_date': 1986})
+
+    #GET movie failure casting director
+    def test_get_movie_failure(self):
+        res = self.client().get('/Movies/123456789', headers={
+            "Authorization": 'bearer ' + self.token_director})
+        body = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(body['success'], False)
+        self.assertEqual(body['message'], 'resource not found')
+
+    #DELETE actor success casting director
+    #DELETE actor failure casting asistant
+    #DELETE movie success producer
+    #DELETE movie failure casting asistant
+    #POST actors success producer
+    #POST actors failure casting asistant
+    #POST movies success producer
+    #POST movies failure casting asistant
+    #PATCH actor success producer
+    #PATCH actor failure casting asistant
+    #PATCH movie success producer
+    #PATCH movie failure casting asistant
+
+if __name__ == "__main__":
+    unittest.main()
